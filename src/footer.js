73373d0bda956b1571ad7a5516ec068fbc728608
Athi Mbondwana <mbondwana.a.s@gmail.com>
2019-10-03 13:21:30 +0200

    This is a to do list

---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+        <div class="navbar">
+  <a href="#home" class="active"> copy rights reserved , terms and conditions &copy; </a>
+
+</div>
+
+    );
+  }
+}
+
+export default Footer;
---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+  render() {
+    return (
+        <div class="navbar">
+  <a href="#home" class="active"> This is my to do list </a>
+  
+
+</div>
+
+    );
+  }
+}
+
+export default Head;
